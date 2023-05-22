aa3808c90e59e5ae8d082503f232f88fb5efe162
Ari laptop <arianne.tran@tum.de>
2023-05-22 08:21:45 +0200

    stf

--- brain tumor segmentation LGG/preprocess.py
+++ brain tumor segmentation LGG/preprocess.py
@@ -1,2 +1,6 @@
 # find data set here https://www.kaggle.com/datasets/mateuszbuda/lgg-mri-segmentation/code
 
+# collect dataset file locations, load in one by one and pass into one big np.array
+# split via skleanr traintestsplit
+# pre-process, normalize by mean and std, but train and test seperately 
+
